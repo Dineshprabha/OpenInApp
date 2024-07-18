254b15495e822f2ffb75f3098166c5b0c593a5e5
Dinesh N <dinesh.prabha1205@gmail.com>
2024-07-18 11:26:29 +0530

    Initial commit

---
+++ app/src/main/java/com/dinesh/openinapp/OpenInApplication.kt
@@ -0,0 +1,8 @@
+package com.dinesh.openinapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class OpenInApplication : Application() {
+}
---
+++ app/src/main/java/com/dinesh/openinapp/dashboard/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dinesh.openinapp.dashboard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dinesh.openinapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
